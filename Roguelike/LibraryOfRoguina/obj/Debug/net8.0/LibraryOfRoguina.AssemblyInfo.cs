b5fe3ff04ce426505ed7b252a1a3424d1b925b5e
FrostPaladin <ifonvizin@gmail.com>
2024-11-03 21:00:37 +0300

    reupload

--- Roguelike/LibraryOfRoguina/obj/Debug/net8.0/LibraryOfRoguina.AssemblyInfo.cs
+++ Roguelike/LibraryOfRoguina/obj/Debug/net8.0/LibraryOfRoguina.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryOfRoguina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448e40798a756ae447787b50ffb5f45a2c40278f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryOfRoguina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryOfRoguina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
