cb1fde857a542cf68edc109eb66e3f6cdadc0d22
FrostPaladin <ifonvizin@gmail.com>
2024-11-03 21:03:33 +0300

    reupload

--- Roguelike/Lib/obj/Debug/net8.0/Lib.AssemblyInfo.cs
+++ Roguelike/Lib/obj/Debug/net8.0/Lib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448e40798a756ae447787b50ffb5f45a2c40278f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fe3ff04ce426505ed7b252a1a3424d1b925b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
